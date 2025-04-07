bf3f6fc3f546fc0cecc34809f80f025cd93d379a
Nikita_pal <161755788+Kthnikita@users.noreply.github.com>
2025-04-07 20:03:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -43,20 +43,20 @@ button.addEventListener("click", async () => {
     favBtn.textContent = "Add to Favourite";
     favBtn.classList.add("fav-btn");
     favBtn.addEventListener("click", () => {
-      addFav(element.id);
+      addFav(element.id, true);
     });
 
     div.appendChild(favBtn);
     box.appendChild(div);
   });
 });
-const favSet = new Set();
-async function addFav(mediaId) {
-  if (favSet.has(mediaId)) {
-    alert("Already in favourites!");
-    return;
-  }
-  favSet.add(mediaId);
+// const favSet = new Set();
+async function addFav(mediaId, addToFav) {
+  // if (favSet.has(mediaId)) {
+  //   alert("Already in favourites!");
+  //   return;
+  // }
+  // favSet.add(mediaId);
   await fetch(`https://api.themoviedb.org/3/account/21921682/favorite`, {
     method: "POST",
     headers: {
@@ -64,39 +64,51 @@ async function addFav(mediaId) {
       "content-type": "application/json",
       Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYTg2OGUxZWEzZDUwMmRkNzg5MmMyYWNiMDFhYzExOSIsIm5iZiI6MTc0MzU4MzA5My43OTEwMDAxLCJzdWIiOiI2N2VjZjc3NWY1YWU3MTQzNWRhYWVjMDQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.YKpBrhpw-a4QSNSMn0DcUtsXGQ0Bm_S3wxH-6mDJ-cM',
     },
-    body: JSON.stringify({ media_type: "movie", media_id: mediaId, favorite: true }),
+    body: JSON.stringify({ media_type: "movie", media_id: mediaId, favorite: addToFav }),
   });
 
-  showFav(mediaId);
+  showFav();
 }
+window.addEventListener("load",()=>{
+  showFav();
+})
 
-async function showFav(showId) {
+async function showFav() {
+  const url = 'https://api.themoviedb.org/3/account/21921682/favorite/movies?language=en-US&page=1&sort_by=created_at.asc';
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYTg2OGUxZWEzZDUwMmRkNzg5MmMyYWNiMDFhYzExOSIsIm5iZiI6MTc0MzU4MzA5My43OTEwMDAxLCJzdWIiOiI2N2VjZjc3NWY1YWU3MTQzNWRhYWVjMDQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.YKpBrhpw-a4QSNSMn0DcUtsXGQ0Bm_S3wxH-6mDJ-cM'
+  }
+};
+
   const favBox = document.querySelector(".fav-list");
-  const response = await fetch(`https://api.themoviedb.org/3/movie/${showId}`, {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYTg2OGUxZWEzZDUwMmRkNzg5MmMyYWNiMDFhYzExOSIsIm5iZiI6MTc0MzU4MzA5My43OTEwMDAxLCJzdWIiOiI2N2VjZjc3NWY1YWU3MTQzNWRhYWVjMDQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.YKpBrhpw-a4QSNSMn0DcUtsXGQ0Bm_S3wxH-6mDJ-cM',
-    },
+  const response = await fetch(url, options);
+  
+  const movies = await response.json();
+  favBox.innerHTML = ""
+  movies.results.reverse().forEach((element) => {
+    const path = element.poster_path
+      ? `https://image.tmdb.org/t/p/w500${element.poster_path}`
+      : "https://via.placeholder.com/100";
+
+    const div = document.createElement("div");
+    div.classList.add("movie-card");
+    div.innerHTML = `
+      <img src="${path}" alt="${element.title}">
+      <p class="movie-title">${element.title}</p>
+      <p class="movie-rating">${element.vote_average}</p>
+    `;
+
+    const favBtn = document.createElement("button");
+    favBtn.textContent = "Remove from Favourite";
+    favBtn.classList.add("fav-btn");
+    favBtn.addEventListener("click", () => {
+      addFav(element.id, false);
+    });
+
+    div.appendChild(favBtn);
+    favBox.appendChild(div);
   });
-
-  const movie = await response.json();
-  const path = movie.poster_path
-    ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
-    : "https://via.placeholder.com/100";
-
-  const div = document.createElement("div");
-  div.classList.add("movie-card");
-  div.innerHTML = `
-    <img src="${path}" alt="${movie.title}">
-    <p class="movie-title">${movie.title}</p>
-    <p class="movie-rating">${movie.vote_average}</p>
-  `;
-  const removeBtn = document.createElement('button');
-  removeBtn.textContent = 'Remove';
-  removeBtn.classList.add('remove-btn');
-  removeBtn.addEventListener('click', () => {
-    div.remove();})
-  div.appendChild(removeBtn);
-  favBox.appendChild(div);
 }
